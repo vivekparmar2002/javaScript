2413bf02ff674b52712c46350a3e9728460c50ea
Vivek Parmar <115700389+vivekparmar2002@users.noreply.github.com>
2023-09-26 09:37:50 +0000

    deta

---
+++ 01Besic/02_detatype.js
@@ -0,0 +1,31 @@
+"use strict" ; //  treat all js code as newer  version 
+
+//     alert (3 + 3)         // we are using node.js not browser
+
+console.log(3+3)   // code repability shode be high
+
+console.log("vivek")
+
+// detatype 
+
+
+let Name ="vivek"
+let age = 22
+let isLoggedIn = false 
+let state ;
+let city = null
+
+// number => 2 to lenth 53
+// bigint => 
+// string =>
+// boolearn => true or fales
+// null
+// undefined => 
+// symbol => unique
+
+
+//object
+
+console.table([Name ,age ,isLoggedIn, state,city])
+
+
